7a64447d9e968778de00fb6b9c2d9f923f55703a
Omar Bassam <omar.bassam88@gmail.com>
2022-08-03 02:13:43 +0200

    Added Disabled Cells Color

--- src/App.vue
+++ src/App.vue
@@ -3,34 +3,41 @@ import { useGame, Color } from "./composables/game.ts";
 
 const { board, update } = useGame();
 function onCellClicked(i, j) {
-	update(i, j, Color.Player);
+  update(i, j, Color.Player);
 }
 </script>
 
 <template>
-	<h1>Connect Four</h1>
-	<div>
-		<div class="row" v-for="(row, rowIndex) of board">
-			<div
-				class="cell"
-				:style="{ backgroundColor: cell.color }"
-				v-for="(cell, cellIndex) of row"
-				@click="onCellClicked(rowIndex, cellIndex)"
-			></div>
-		</div>
-	</div>
+  <h1>Connect Four</h1>
+  <div>
+    <div class="row" v-for="(row, rowIndex) of board">
+      <div
+        class="cell"
+        :style="{
+          backgroundColor:
+            cell.color != Color.Empty
+              ? cell.color
+              : cell.allowed
+              ? Color.Empty
+              : 'gray',
+        }"
+        v-for="(cell, cellIndex) of row"
+        @click="onCellClicked(rowIndex, cellIndex)"
+      ></div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
 .row {
-	display: flex;
+  display: flex;
 }
 
 .cell {
-	margin: 0.5rem;
-	width: 2rem;
-	height: 2rem;
-	background-color: white;
-	border-radius: 2rem;
+  margin: 0.5rem;
+  width: 2rem;
+  height: 2rem;
+  background-color: white;
+  border-radius: 2rem;
 }
 </style>
