9972695f110a59896708909b138502dfa550973b
Omar Bassam <omar.bassam88@gmail.com>
2022-08-03 12:24:52 +0200

    Fixing Winning Checking

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,69 @@
 <script setup lang="ts">
+import { computed } from 'vue'
 import { useGame, Color } from "./composables/game.ts";
 
-const { board, update } = useGame();
+const { board, update, turn, winner } = useGame();
+const winnerMsg = computed<string>(() => {
+	switch (winner.value) {
+		case "player":
+			return "Congratulations You Won!"
+		case "ai":
+			return "I can't believe you were beaten by a piece of metal."
+		case "tie":
+			return "Well, at least it's a Tie."
+		default:
+			return ""
+	}
+})
+
 function onCellClicked(i, j) {
-  update(i, j, Color.Player);
+	if (!turn.value || winner.value) return;
+	update(i, j, Color.Player);
 }
 </script>
 
 <template>
-  <h1>Connect Four</h1>
-  <div>
-    <div class="row" v-for="(row, rowIndex) of board">
-      <div
-        class="cell"
-        :style="{
-          backgroundColor:
-            cell.color != Color.Empty
-              ? cell.color
-              : cell.allowed
-              ? Color.Empty
-              : 'gray',
-        }"
-        v-for="(cell, cellIndex) of row"
-        @click="onCellClicked(rowIndex, cellIndex)"
-      ></div>
-    </div>
-  </div>
+	<h1>Connect Four</h1>
+	<div class="board">
+		<div class="row" v-for="(row, rowIndex) of board">
+			<div class="cell" :style="{
+				backgroundColor:
+					cell.color != Color.Empty
+						? cell.color
+						: cell.allowed
+							? Color.Empty
+							: 'gray',
+			}" v-for="(cell, cellIndex) of row" @click="onCellClicked(rowIndex, cellIndex)"></div>
+		</div>
+	</div>
+	<div v-if="winner">
+		{{ winnerMsg }}
+	</div>
+	<div v-else>
+		{{ "It's " + (turn ? "Your" : "AI") + " turn now." }}
+	</div>
 </template>
 
 <style scoped>
+.board {
+	background-color: #3232cb;
+	padding-top: 1rem;
+	padding-bottom: 1rem;
+
+}
+
 .row {
-  display: flex;
+	display: flex;
+	justify-items: center;
+	justify-content: center;
+	margin: auto;
 }
 
 .cell {
-  margin: 0.5rem;
-  width: 2rem;
-  height: 2rem;
-  background-color: white;
-  border-radius: 2rem;
+	margin: 0.5rem;
+	width: 2rem;
+	height: 2rem;
+	background-color: white;
+	border-radius: 2rem;
 }
 </style>
--- src/composables/game.ts
+++ src/composables/game.ts
@@ -1,168 +1,249 @@
 import { ref, watch } from "vue";
+import type { Ref } from 'vue'
 
 export enum Color {
-  Player = "red",
-  AI = "blue",
-  Empty = "white",
+	Player = "red",
+	AI = "yellow",
+	Empty = "white",
 }
 
 export interface Cell {
-  allowed: boolean;
-  color: Color;
+	allowed: boolean;
+	color: Color;
 }
 
+type Board = Cell[][]
+type Winner = "player" | "ai" | "tie" | null;
+
 const numRows: number = 7;
-const numCols: number = 12;
-const emptyBoard: Cell[][] = new Array(numRows)
-  .fill(0)
-  .map((_, row) =>
-    new Array(numCols)
-      .fill(0)
-      .map((_) => ({ color: Color.Empty, allowed: row === numRows - 1 }))
-  );
-const board = ref(emptyBoard);
-const turn = ref(true);
+const numCols: number = 6;
+const emptyBoard: Board = new Array(numRows)
+	.fill(0)
+	.map((_, row) =>
+		new Array(numCols)
+			.fill(0)
+			.map((_) => ({ color: Color.Empty, allowed: row === numRows - 1 }))
+	);
+const board: Ref<Board> = ref<Board>(emptyBoard);
+const turn: Ref<boolean> = ref(true);
+const winner: Ref<Winner> = ref<Winner>(null)
 
 function update(row: number, col: number, color: Color) {
-  if (!board.value[row][col].allowed) return;
-  board.value[row][col].color = color;
-  board.value[row][col].allowed = false;
-  if (row >= 1) board.value[row - 1][col].allowed = true;
-  turn.value = !turn.value;
+	if (!board.value[row][col].allowed || winner.value) return;
+	board.value[row][col].color = color;
+	board.value[row][col].allowed = false;
+	if (row >= 1) board.value[row - 1][col].allowed = true;
+	winner.value = checkWinner();
+	if (!winner.value) turn.value = !turn.value;
+}
+
+function checkWinner(): Winner {
+	const isLeftFourSame = (i: number, j: number) => {
+		if (j < 3) return false
+		let x = board.value[i][j].color
+		if (board.value[i][j - 1].color != x) return false
+		if (board.value[i][j - 2].color != x) return false
+		if (board.value[i][j - 3].color != x) return false
+		return true
+	}
+	const isRightFourSame = (i, j) => {
+		if (j > numCols - 4) return false
+		let x = board.value[i][j].color
+		if (board.value[i][j + 1].color != x) return false
+		if (board.value[i][j + 2].color != x) return false
+		if (board.value[i][j + 3].color != x) return false
+		return true
+	}
+	const isBottomFourSame = (i, j) => {
+		if (i > numRows - 4) return false
+		let x = board.value[i][j].color
+		if (board.value[i + 1][j].color != x) return false
+		if (board.value[i + 2][j].color != x) return false
+		if (board.value[i + 3][j].color != x) return false
+		return true
+	}
+	const isLeftDiagonalFourSame = (i, j) => {
+		if (j > numCols - 4 || i > numRows - 4) return false
+		let x = board.value[i][j].color
+		if (board.value[i + 1][j + 1].color != x) return false
+		if (board.value[i + 2][j + 2].color != x) return false
+		if (board.value[i + 3][j + 3].color != x) return false
+		return true
+	}
+	const isRightDiagonalFourSame = (i, j) => {
+		if (j < 3 || i > numRows - 4) return false
+		let x = board.value[i][j].color
+		if (board.value[i + 1][j - 1].color != x) return false
+		if (board.value[i + 2][j - 2].color != x) return false
+		if (board.value[i + 3][j - 3].color != x) return false
+		return true
+	}
+	const possibleCases = [isLeftFourSame, isRightFourSame, isBottomFourSame, isLeftDiagonalFourSame, isRightDiagonalFourSame]
+
+	// Check for possible cases to win
+	for (let i = 0; i < numRows; i++) {
+		for (let j = 0; j < numCols; j++) {
+			if (board.value[i][j].color != Color.Empty) {
+				for (let k = 0; k < possibleCases.length; k++) {
+					if (possibleCases[k](i, j)) {
+						return board.value[i][j].color == Color.Player ? "player" : "ai"
+					}
+				}
+			}
+		}
+	}
+
+	// Check if all cells are filled
+	for (let i = 0; i < numRows; i++) {
+		for (let j = 0; j < numCols; j++) {
+			if (board.value[i][j].allowed) return null
+		}
+	}
+
+	return "tie"
 }
 
 function score(color: Color): number {
-  const horizontalScore = (i: number, j: number) => {
-    let count = 0;
-    if (j < numCols && board.value[i][j++].color == color) count++;
-    if (j < numCols && board.value[i][j++].color == color) count++;
-    if (j < numCols && board.value[i][j++].color == color) count++;
-    if (j < numCols && board.value[i][j++].color == color) count++;
-    return points[count];
-  };
-  const verticalScore = (i: number, j: number) => {
-    let count = 0;
-    if (i < numRows && board.value[i++][j].color == color) count++;
-    if (i < numRows && board.value[i++][j].color == color) count++;
-    if (i < numRows && board.value[i++][j].color == color) count++;
-    if (i < numRows && board.value[i++][j].color == color) count++;
-    return points[count];
-  };
-  const leftDiagonalScore = (i: number, j: number) => {
-    let count = 0;
-    if (i < numRows && j < numCols && board.value[i++][j++].color == color)
-      count++;
-    if (i < numRows && j < numCols && board.value[i++][j++].color == color)
-      count++;
-    if (i < numRows && j < numCols && board.value[i++][j++].color == color)
-      count++;
-    if (i < numRows && j < numCols && board.value[i++][j++].color == color)
-      count++;
-    return points[count];
-  };
+	const horizontalScore = (i: number, j: number) => {
+		let count = 0;
+		if (j < numCols && board.value[i][j++].color == color) count++;
+		if (j < numCols && board.value[i][j++].color == color) count++;
+		if (j < numCols && board.value[i][j++].color == color) count++;
+		if (j < numCols && board.value[i][j++].color == color) count++;
+		return points[count];
+	};
+	const verticalScore = (i: number, j: number) => {
+		let count = 0;
+		if (i < numRows && board.value[i++][j].color == color) count++;
+		if (i < numRows && board.value[i++][j].color == color) count++;
+		if (i < numRows && board.value[i++][j].color == color) count++;
+		if (i < numRows && board.value[i++][j].color == color) count++;
+		return points[count];
+	};
+	const leftDiagonalScore = (i: number, j: number) => {
+		let count = 0;
+		if (i < numRows && j < numCols && board.value[i++][j++].color == color)
+			count++;
+		if (i < numRows && j < numCols && board.value[i++][j++].color == color)
+			count++;
+		if (i < numRows && j < numCols && board.value[i++][j++].color == color)
+			count++;
+		if (i < numRows && j < numCols && board.value[i++][j++].color == color)
+			count++;
+		return points[count];
+	};
 
-  const rightDiagonalScore = (i: number, j: number) => {
-    let count = 0;
-    if (i < numRows && j < numCols && board.value[i++][j--].color == color)
-      count++;
-    if (i < numRows && j < numCols && board.value[i++][j--].color == color)
-      count++;
-    if (i < numRows && j < numCols && board.value[i++][j--].color == color)
-      count++;
-    if (i < numRows && j < numCols && board.value[i++][j--].color == color)
-      count++;
-    return points[count];
-  };
-  let total = 0;
-  const points = [0, 1, 2, 25, 100];
+	const rightDiagonalScore = (i: number, j: number) => {
+		let count = 0;
+		if (i < numRows && j < numCols && board.value[i++][j--].color == color)
+			count++;
+		if (i < numRows && j < numCols && board.value[i++][j--].color == color)
+			count++;
+		if (i < numRows && j < numCols && board.value[i++][j--].color == color)
+			count++;
+		if (i < numRows && j < numCols && board.value[i++][j--].color == color)
+			count++;
+		return points[count];
+	};
+	let total = 0;
+	const points = [0, 1, 2, 25, 100];
 
-  for (let i = 0; i < numRows; ++i) {
-    for (let j = 0; j < numCols; j++) {
-      if (board.value[i][j] == color) {
-        total += horizontalScore(i, j);
-        total += verticalScore(i, j);
-        total += leftDiagonalScore(i, j);
-        total += rightDiagonalScore(i, j);
-        total -= 3;
-      }
-    }
-  }
-  return total;
+	for (let i = 0; i < numRows; ++i) {
+		for (let j = 0; j < numCols; j++) {
+			if (board.value[i][j] == color) {
+				total += horizontalScore(i, j);
+				total += verticalScore(i, j);
+				total += leftDiagonalScore(i, j);
+				total += rightDiagonalScore(i, j);
+				total -= 3;
+			}
+		}
+	}
+	return total;
 }
 
 function minimax(depth: number, maximize: boolean): number {
-  if (depth <= 1) return score(Color.AI) - score(Color.Player);
-  if (maximize) {
-    let maxVal: number = -Infinity;
-    for (let row = numRows - 1; row >= 0; row--) {
-      for (let col = 0; col < numCols; col++) {
-        if (board.value[row][col].allowed) {
-          board.value[row][col] = { color: Color.AI, allowed: false };
-          if (row > 0) board.value[row - 1][col].allowed = true;
-          let val: number = minimax(depth - 1, false);
-          maxVal = Math.max(maxVal, val);
-          board.value[row][col] = { color: Color.Empty, allowed: true };
-          if (row > 0) board.value[row - 1][col].allowed = false;
-        }
-      }
-    }
-    return maxVal;
-  } else {
-    let minVal: number = Infinity;
-    for (let row = numRows - 1; row >= 0; row--) {
-      for (let col = 0; col < numCols; col++) {
-        if (board.value[row][col].allowed) {
-          board.value[row][col] = { color: Color.Player, allowed: false };
-          if (row > 0) board.value[row - 1][col].allowed = true;
-          let val: number = minimax(depth - 1, true);
-          minVal = Math.min(minVal, val);
-          board.value[row][col] = { color: Color.Empty, allowed: true };
-          if (row > 0) board.value[row - 1][col].allowed = false;
-        }
-      }
-    }
-    return minVal;
-  }
+	let win = checkWinner();
+	if (win == "player") return -200;
+	if (win == "ai") return 200;
+
+	if (depth < 1) return score(Color.AI) - score(Color.Player);
+	if (maximize) {
+		let maxVal: number = -Infinity;
+		for (let col = 0; col < numCols; col++) {
+			for (let row = numRows - 1; row >= 0; row--) {
+				if (board.value[row][col].allowed) {
+					board.value[row][col] = { color: Color.AI, allowed: false };
+					if (row > 0) board.value[row - 1][col].allowed = true;
+					let val: number = minimax(depth - 1, false);
+					maxVal = Math.max(maxVal, val);
+					board.value[row][col] = { color: Color.Empty, allowed: true };
+					if (row > 0) board.value[row - 1][col].allowed = false;
+				}
+			}
+		}
+		return maxVal;
+	} else {
+		let minVal: number = Infinity;
+		for (let col = 0; col < numCols; col++) {
+			for (let row = numRows - 1; row >= 0; row--) {
+				if (board.value[row][col].allowed) {
+					board.value[row][col] = { color: Color.Player, allowed: false };
+					if (row > 0) board.value[row - 1][col].allowed = true;
+					let val: number = minimax(depth - 1, true);
+					minVal = Math.min(minVal, val);
+					board.value[row][col] = { color: Color.Empty, allowed: true };
+					if (row > 0) board.value[row - 1][col].allowed = false;
+				}
+			}
+		}
+		return minVal;
+	}
 }
 
 function playBot() {
-  let maxVal = -Infinity;
-  let i: number;
-  let j: number;
-  for (let row = numRows - 1; row >= 0; row--) {
-    for (let col = 0; col < numCols; col++) {
-      if (board.value[row][col].allowed) {
-        board.value[row][col] = { color: Color.AI, allowed: false };
-        if (row > 0) board.value[row - 1][col].allowed = true;
+	let maxVal = -Infinity;
+	let i: number;
+	let j: number;
+	for (let col = 0; col < numCols; col++) {
+		for (let row = numRows - 1; row >= 0; row--) {
+			if (board.value[row][col].allowed) {
+				// Play cell at row, col
+				board.value[row][col] = { color: Color.AI, allowed: false };
+				if (row > 0) board.value[row - 1][col].allowed = true;
 
-        let val = minimax(3, false);
+				let val = minimax(5, true);
 
-        board.value[row][col] = { color: Color.Empty, allowed: true };
-        if (row > 0) board.value[row - 1][col].allowed = false;
+				// Undo Last Move
+				board.value[row][col] = { color: Color.Empty, allowed: true };
+				if (row > 0) board.value[row - 1][col].allowed = false;
 
-        if (val > maxVal) {
-          maxVal = val;
-          i = row;
-          j = col;
-        }
-      }
-    }
-  }
-  update(i, j, Color.AI);
+				if (val > maxVal) {
+					maxVal = val;
+					i = row;
+					j = col;
+				}
+
+				console.log(maxVal)
+			}
+		}
+	}
+	update(i, j, Color.AI);
 }
 
 watch(turn, () => {
-  if (!turn.value) {
-    setTimeout(() => {
-      playBot();
-    }, 0);
-  }
+	if (!turn.value && !winner.value) {
+		setTimeout(() => {
+			playBot();
+		}, 0);
+	}
 });
 
 export function useGame() {
-  return {
-    board,
-    update,
-    turn,
-  };
+	return {
+		board,
+		update,
+		turn,
+		winner
+	};
 }
